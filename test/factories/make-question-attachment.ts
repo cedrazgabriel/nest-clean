a166a1799d5515a9f3c6dce90a44d9bc63de1b29
Gabriel Cedraz <cedrazdev@gmail.com>
2024-05-24 13:35:41 -0300

    feat: alter edit question e2e test to persist attachmentsIds

--- test/factories/make-question-attachment.ts
+++ test/factories/make-question-attachment.ts
@@ -1,3 +1,5 @@
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 import { UniqueEntityId } from 'src/core/entities/unique-entity-id'
 import {
   QuestionAttachment,
@@ -19,4 +21,26 @@ export function makeQuestionAttachment(
   )
 
   return questionAttachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<QuestionAttachmentProps> = {},
+  ): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString(),
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
