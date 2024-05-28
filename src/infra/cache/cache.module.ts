71bba71fc594ecd98703f0a0a028e088989770ea
Gabriel Cedraz <cedrazdev@gmail.com>
2024-05-28 16:58:44 -0300

    feat: implemente redis repository

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { EnvModule } from '../env/env.module'
-import { EnvService } from '../env/env.service'
+import { ICacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
+import { RedisService } from './redis/redis.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: ICacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [ICacheRepository],
 })
 export class CacheModule {}
---
+++ src/infra/cache/redis/redis-cache-repository.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common'
+import { ICacheRepository } from '../cache-repository'
+import { RedisService } from './redis.service'
+
+@Injectable()
+export class RedisCacheRepository implements ICacheRepository {
+  constructor(private redis: RedisService) {}
+  async set(key: string, value: string) {
+    await this.redis.set(key, value, 'EX', 60 * 15)
+  }
+
+  get(key: string): Promise<string | null> {
+    return this.redis.get(key)
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key)
+  }
+}
